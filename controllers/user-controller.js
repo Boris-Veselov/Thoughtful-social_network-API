f589c5f4a44c5b4789d3ecb63a26f9540b02877c
Boris Veselov <borisveselov@yahoo.com>
2022-05-28 12:59:56 -0400

    updated user controller

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -42,7 +42,59 @@ const userController = {
             res.status(400).json(err)
         })
     },
-    
+
+    // Update User by id
+    updateUser({params, body}, res) {
+        User.findOneAndUpdate({_id: params.id}, body, {new: true, runValidators: true})
+        .then(dbUserData => {
+            if(!dbUserData) {
+                res.status(404).json({message: 'No such User with this id!'});
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => res.json(err))
+    },
+
+    // delete User
+    deleteUser({params}, res) {
+        User.findOneAndDelete({_id: params.id})
+        .then(dbUserData => {
+            if(!dbUserData) {
+                res.status(404).json({message: 'No such User with this id!'});
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => res.status(400).json(err));
+    },
+
+    // add new Friend
+    addFriend({ params }, res) {
+        User.findOneAndUpdate(
+            {_id: params.userId},
+            { $push: { friends: params.friendId } },
+            { new: true, runValidators: true}
+        )
+        .then(dbUserData => {
+            if (!dbUserData) {
+                res.status(404).json({ message: 'No such User with this id!' });
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => res.json(err));
+    },
+    // delete Friend
+    removeFriend( { params }, res) {
+        User.findOneAndUpdate(
+            { _id: params.userId },
+            { $pull: { friends: params.friendId }},
+            { new: true}
+        )
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => res.json(err));
+    }
 };
 
 module.exports = userController;
