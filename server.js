b3d15ed9bbd0c9a3735c6dca79ccbb33fe9fdca4
Boris Veselov <borisveselov@yahoo.com>
2022-05-29 13:04:11 -0400

    updates to server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/thoughtful-social_network-api', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/t-sn-api', {
   useNewUrlParser: true,
   useUnifiedTopology: true 
 }); 
