41b244b92ea8044c546db2a75641124a8c5b392e
Boris Veselov <borisveselov@yahoo.com>
2022-05-29 11:50:06 -0400

    updated server.js

--- server.js
+++ server.js
@@ -4,9 +4,12 @@ const mongoose = require('mongoose');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/thoughtful-social_network-api', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/thoughtful-social_network-api', {
   useNewUrlParser: true,
   useUnifiedTopology: true 
 }); 
+
+// this is used to log mongo queries to see that they are being executed
+mongoose.set('debug', true);
 
 app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
